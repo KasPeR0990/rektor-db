286aef5db86e0a682035d5e11a11a179370e4067
Kasper Hallenius <kasper.hallenius@kasper.halleniusLCXX2FN4NF>
2025-09-18 11:05:04 +0200

    idk notebook

---
+++ test.py
@@ -0,0 +1,13 @@
+import numpy as np 
+
+vector_column = np.array([[1], [2], [3]])
+print(vector_column);
+
+mat1 = np.matrix("1, 2, 3, 4; 4, 5, 6, 7; 7, 8, 9; 9")
+print(mat1)
+
+
+
+print(mat1.shape[0])
+
+
